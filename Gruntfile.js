107be8cb1b265ad7ea71ab0f87a224fa752d5cf3
wilz5363 <chanwilson1993@gmail.com>
2016-09-01 21:55:54 +0800

    Preparing grunt

---
+++ Gruntfile.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Wilson on 9/1/2016.
+ */
+module.exports = function (grunt) {
+    grunt.registerTask('default', function () {
+        grunt.log.write('Logging...').ok();
+    })
+}
